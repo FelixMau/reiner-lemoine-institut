48551fd92bfafc236c393e161c9c1e2a4055b032
FelixMau <98911066+FelixMau@users.noreply.github.com>
2022-02-02 22:58:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-import pandas as pd
-
 # Todo:
 #    1. Importiere die mitgelieferte CSV-Datei in ein Pandas DataFrame, wobei du den Spaltennamen B in „Energiequelle“,
 #       den Spaltennamen C in „Leistung in kW“ umbenennst und die Spalte A löschst. In der CSV-Datei entspricht jeder
@@ -12,46 +9,17 @@ import pandas as pd
 #    6. Plotte die Leistung der Energiequelle „gas“ in Abhängigkeit von der Anzahl der Einträge als Graph
 #       (z.B. als Balkendiagramm) und speichere sie als jpg/png-Grafik..
 
-##################################################
-# Aufgabe 1
-with open(os.getcwd() + "/2022-02-02 Data Bewerbungsaufgabe.csv") as f:
-    df = pd.read_csv(f, delimiter=";")
-df = df.rename(columns={"generator:source": "Energiequelle", "generator:output": "Leistung in kW"})
-df.pop("wkt_geom")
+import Aufgabe1bis5
+import Aufgabe6
 
-##################################################
-# Aufgabe 2
-df.sort_values(by=["Energiequelle"], inplace=True)
+def main():
+    df = Aufgabe1bis5.import_ajustments()
+    df = Aufgabe1bis5.sort(df)
+    df = Aufgabe1bis5.yes_to_value(df)
+    df = Aufgabe1bis5.remove_nan_and_invalid(df)
+    Aufgabe1bis5.split_dataframe_by_source_and_save(df)
+    Aufgabe6.visualize()
 
-##################################################
-# Aufgabe 3
-df["Leistung in kW"] = df["Leistung in kW"].replace(to_replace="yes", value="600")
 
-##################################################
-# Aufgabe 4
-# Remove Nan values:
-drop_arr = df[df["Leistung in kW"].isna()]
-df = df.drop(drop_arr.index)
-
-# Remove "non figure values"
-drop_arr = []
-for i in df.index:
-    try:
-        float(df["Leistung in kW"][i])
-    except ValueError:
-        drop_arr.append(i)
-df = df.drop(drop_arr)
-
-##################################################
-# Aufgabe 5
-try:
-    os.makedirs("Energiequellen")
-except FileExistsError:
-    pass
-
-for i in df.groupby(["Energiequelle"]):
-    for k in i:
-        if type(k) == pd.DataFrame:
-            k = k.reset_index()
-            k.pop("index")
-            k.to_csv("Energiequellen/" + i[0] + ".csv", index=False, sep=";")
+if __name__ == '__main__':
+    main()
